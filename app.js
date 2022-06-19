e2781c75399cf672c49ca659bd935fc4dbfddaa8
Shubhan Yenuganti <yrshubhan@gmail.com>
2022-06-19 16:49:19 -0700

    Updated home.js
    Added fetch for api that causes player data to appear in table when inputted from form
    
    Added random player generator to generate player that the user should guess to

--- app.js
+++ app.js
@@ -28,4 +28,4 @@ app.use((req, res, next) => {
     })
 });
 
-app.listen(3000);
+app.listen(4000);
--- routes/home.js
+++ routes/home.js
@@ -1,29 +1,289 @@
+const fetch = require('node-fetch')
+
+var requestOptions = {
+  method: 'GET',
+  redirect: 'follow'
+};
+
 const path = require('path');
 
 const express = require('express');
 
 const rootDir = require('../util/path');
+const {
+  json
+} = require('body-parser');
 
 const router = express.Router();
 
 const player = [];
+const player_names = [];
+
+const answer = [];
+const pos = [
+  'QB',
+  'RB',
+  'WR',
+  'TE',
+  'T',
+  'G',
+  'C',
+  'DE',
+  'DT',
+  'LB',
+  'CB',
+  'S',
+  'PK',
+  'P'
+]
+
+const random_pos = pos[Math.floor(Math.random() * 14)];
+
+fetch("https://ecb5-73-93-10-13.ngrok.io/all/" + random_pos, requestOptions)
+  .then(response => response.text())
+  .then(
+    result => updateGuess(JSON.parse(result))
+  )
+  .catch(
+    error => console.log("API request failed or you didn't enter a valid input")
+  );
+
+const updateGuess = (data) => {
+
+  // Conference (0 or 1)
+  const rand_conf = Math.floor(Math.random() * 2)
+  // Division (0,1,2,3)
+  const rand_div = Math.floor(Math.random() * 4)
+  // Team (0,1,2,3)
+  const rand_team = Math.floor(Math.random() * 4)
+
+  // IF WR:
+  // Player (0,1,2)
+  if (random_pos == "WR") {
+    const rand_player = Math.floor(Math.random() * 3)
+    answer[0] = data[rand_conf][rand_div][rand_team][rand_player]
+  }
+
+  // IF T, G, DE, CB, S:
+  // Player (0,1)
+
+  if (random_pos == "T" || random_pos == "G" || random_pos == "DE" || random_pos == "CB" || random_pos == "S") {
+    const rand_player = Math.floor(Math.random() * 2)
+
+    answer[0] = data[rand_conf][rand_div][rand_team][rand_player]
+  }
+
+  // DT
+  // Teams w/NT -- Player (0)
+  // Teams w/out NT -- Player (0,1)
+
+  if (random_pos == "DT") {
+    if (rand_conf == 0) {
+      if (rand_div == 0) {
+        if (rand_team == 1) {
+          answer[0] = data[rand_conf][rand_div][rand_team]
+        } else {
+          const rand_player = Math.floor(Math.random() * 2)
+          answer[0] = data[rand_conf][rand_div][rand_team][rand_player]
+        }
+      } else if (rand_div == 1) {
+        if (rand_team == 0 || rand_team == 3) {
+          answer[0] = data[rand_conf][rand_div][rand_team]
+        } else {
+          const rand_player = Math.floor(Math.random() * 2)
+          answer[0] = data[rand_conf][rand_div][rand_team][rand_player]
+        }
+      } else if (rand_div == 2) {
+        if (rand_team == 0 || rand_team == 3) {
+          answer[0] = data[rand_conf][rand_div][rand_team]
+        } else {
+          const rand_player = Math.floor(Math.random() * 2)
+          answer[0] = data[rand_conf][rand_div][rand_team][rand_player]
+        }
+      } else if (rand_div == 3) {
+        if (rand_team == 2 || rand_team == 3) {
+          answer[0] = data[rand_conf][rand_div][rand_team]
+        } else {
+          const rand_player = Math.floor(Math.random() * 2)
+          answer[0] = data[rand_conf][rand_div][rand_team][rand_player]
+        }
+      }
+    } else if (rand_conf == 1) {
+      if (rand_div == 0) {
+        if (rand_team == 1) {
+          answer[0] = data[rand_conf][rand_div][rand_team]
+        } else {
+          const rand_player = Math.floor(Math.random() * 2)
+          answer[0] = data[rand_conf][rand_div][rand_team][rand_player]
+        }
+      } else if (rand_div == 1) {
+        if (rand_team == 0 || rand_team == 1) {
+          answer[0] = data[rand_conf][rand_div][rand_team]
+        } else {
+          const rand_player = Math.floor(Math.random() * 2)
+          answer[0] = data[rand_conf][rand_div][rand_team][rand_player]
+        }
+      } else if (rand_div == 2) {
+        if (rand_team == 2 || rand_team == 3) {
+          answer[0] = data[rand_conf][rand_div][rand_team]
+        } else {
+          const rand_player = Math.floor(Math.random() * 2)
+          answer[0] = data[rand_conf][rand_div][rand_team][rand_player]
+        }
+      } else if (rand_div == 3) {
+        if (rand_team == 0 || rand_team == 3) {
+          answer[0] = data[rand_conf][rand_div][rand_team]
+        } else {
+          const rand_player = Math.floor(Math.random() * 2)
+          answer[0] = data[rand_conf][rand_div][rand_team][rand_player]
+        }
+      }
+    }
+  }
+
+
+  // LB
+  // Teams w/NT -- Player (0,1,2,3)
+  // Teams w/out NT -- Player (0,1,2)
+
+  if (random_pos == "LB") {
+    if (rand_conf == 0) {
+      if (rand_div == 0) {
+        if (rand_team == 1) {
+          const rand_player = Math.floor(Math.random() * 4)
+          answer[0] = data[rand_conf][rand_div][rand_team][rand_player]
+        } else {
+          const rand_player = Math.floor(Math.random() * 3)
+          answer[0] = data[rand_conf][rand_div][rand_team][rand_player]
+        }
+      } else if (rand_div == 1) {
+        if (rand_team == 0 || rand_team == 3) {
+          const rand_player = Math.floor(Math.random() * 4)
+          answer[0] = data[rand_conf][rand_div][rand_team][rand_player]
+        } else {
+          const rand_player = Math.floor(Math.random() * 3)
+          answer[0] = data[rand_conf][rand_div][rand_team][rand_player]
+        }
+      } else if (rand_div == 2) {
+        if (rand_team == 0 || rand_team == 3) {
+          const rand_player = Math.floor(Math.random() * 4)
+          answer[0] = data[rand_conf][rand_div][rand_team][rand_player]
+        } else {
+          const rand_player = Math.floor(Math.random() * 3)
+          answer[0] = data[rand_conf][rand_div][rand_team][rand_player]
+        }
+      } else if (rand_div == 3) {
+        if (rand_team == 2 || rand_team == 3) {
+          const rand_player = Math.floor(Math.random() * 4)
+          answer[0] = data[rand_conf][rand_div][rand_team][rand_player]
+        } else {
+          const rand_player = Math.floor(Math.random() * 3)
+          answer[0] = data[rand_conf][rand_div][rand_team][rand_player]
+        }
+      }
+    } else if (rand_conf == 1) {
+      if (rand_div == 0) {
+        if (rand_team == 1) {
+          const rand_player = Math.floor(Math.random() * 4)
+          answer[0] = data[rand_conf][rand_div][rand_team][rand_player]
+        } else {
+          const rand_player = Math.floor(Math.random() * 3)
+          answer[0] = data[rand_conf][rand_div][rand_team][rand_player]
+        }
+      } else if (rand_div == 1) {
+        if (rand_team == 0 || rand_team == 1) {
+          const rand_player = Math.floor(Math.random() * 4)
+          answer[0] = data[rand_conf][rand_div][rand_team][rand_player]
+        } else {
+          const rand_player = Math.floor(Math.random() * 3)
+          answer[0] = data[rand_conf][rand_div][rand_team][rand_player]
+        }
+      } else if (rand_div == 2) {
+        if (rand_team == 2 || rand_team == 3) {
+          const rand_player = Math.floor(Math.random() * 4)
+          answer[0] = data[rand_conf][rand_div][rand_team][rand_player]
+        } else {
+          const rand_player = Math.floor(Math.random() * 3)
+          answer[0] = data[rand_conf][rand_div][rand_team][rand_player]
+        }
+      } else if (rand_div == 3) {
+        if (rand_team == 0 || rand_team == 3) {
+          const rand_player = Math.floor(Math.random() * 4)
+          answer[0] = data[rand_conf][rand_div][rand_team][rand_player]
+        } else {
+          const rand_player = Math.floor(Math.random() * 3)
+          answer[0] = data[rand_conf][rand_div][rand_team][rand_player]
+        }
+      }
+    }
+  }
+
+  if (random_pos == "QB" || random_pos == "TE" || random_pos == "RB" || random_pos == "C" || random_pos == "PK" || random_pos == "P") {
+    answer[0] = data[rand_conf][rand_div][rand_team]
+  }
+  
+  console.log(answer[0]);
+}
 
 // add-product => GET
 router.get('/', (req, res, next) => {
   res.render('home', {
     players: player,
     pageTitle: 'home',
-    path: '/'
+    path: '/',
+    guess: answer[0]
   })
 });
 
 // /admin/add-product => POST
 router.post('/', (req, res, next) => {
-  player.push({
-    name: req.body.player
-  });
-  res.redirect('/');
-});
+    // Get data from https://73b7-73-93-10-13.ngrok.io (note need to change url when api stops running & reruns)
+
+    fetch("https://ecb5-73-93-10-13.ngrok.io/" + req.body.player, requestOptions)
+      .then(response => response.text())
+      .then(
+        result => updatePlayer(JSON.parse(result))
+      )
+      .catch(
+        error => console.log("API request failed or you didn't enter a valid input")
+      );
+
+    const updatePlayer = (data) => {
+
+      var today = new Date();
+      var date2 = new Date(data.DOB)
+
+      var diff = (today.getTime() - date2.getTime()) / 1000;
+      diff /= (60 * 60 * 24);
+      var age = Math.abs(Math.round(diff / 365.25));
+
+      var addPlayer = false
+
+      if (player_names.includes(req.body.player) && player.length != 0) {
+        console.log("SAME PLAYER IN ARRAY")
+      } else {
+        player_names.push(data.name)
+        addPlayer = true
+      }
+
+      if (addPlayer) {
+        player.push({
+          name: req.body.player,
+          team: data.team,
+          pos: data.position,
+          div: data.division,
+          height: data.HtWt.slice(0, data.HtWt.indexOf(",")) + " ft",
+          weight: data.HtWt.slice(data.HtWt.indexOf(",") + 2),
+          age: age,
+          jersey: data.jersey
+        });
+      }
+      res.redirect('/');
+    }
+  }
+
+);
+
 
 exports.routes = router;
 exports.players = player;
